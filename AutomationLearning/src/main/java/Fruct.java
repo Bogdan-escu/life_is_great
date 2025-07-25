95db13ff32b7c3e59476f8c1968ba1c673764a7b
Bogdan-escu <preda_bogdan2002@yahoo.com>
2025-07-25 15:42:58 +0300

    new repo

---
+++ AutomationLearning/src/main/java/Fruct.java
@@ -0,0 +1,50 @@
+public class Fruct {
+
+    private String numeFruct;
+    private String culoareFruct;
+    private String textureFruct;
+    private String gustFruct;
+    private String mirosFruct;
+    private String greutateFruct;
+
+    public void setNumeFruct(String mar){
+        this.numeFruct = mar;
+    }
+    public String getNumeFruct(){
+        return numeFruct;
+    }
+
+    public java.lang.String getTextureFruct() {
+        return textureFruct;
+    }
+
+    public void setTextureFruct(java.lang.String textureFruct) {
+        this.textureFruct = textureFruct;
+    }
+
+    public java.lang.String getGustFruct() {
+        return gustFruct;
+    }
+
+    public void setGustFruct(java.lang.String gustFruct) {
+        this.gustFruct = gustFruct;
+    }
+
+    public java.lang.String getMirosFruct() {
+        return mirosFruct;
+    }
+
+    public void setMirosFruct(java.lang.String mirosFruct) {
+        this.mirosFruct = mirosFruct;
+    }
+
+    public java.lang.String getGreutateFruct() {
+        return greutateFruct;
+    }
+
+    public void setGreutateFruct(java.lang.String greutateFruct) {
+        this.greutateFruct = greutateFruct;
+    }
+}
+
+
